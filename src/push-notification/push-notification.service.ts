821ab8cdd2e2b553244a9abd56e4af7028165243
ivanstatkevich <statkevich.ivan999@gmail.com>
2022-11-25 09:05:15 +0200

    finished working on push notifications

--- src/push-notification/push-notification.service.ts
+++ src/push-notification/push-notification.service.ts
@@ -3,6 +3,7 @@ import {Cron} from '@nestjs/schedule';
 import {InjectModel} from '@nestjs/mongoose';
 import {Task, TaskDocument} from '../tasks/tasks.schema';
 import {Model} from 'mongoose';
+import Expo, {ExpoPushMessage} from 'expo-server-sdk';
 
 @Injectable()
 export class PushNotificationService {
@@ -13,21 +14,42 @@ export class PushNotificationService {
 
   @Cron('* * * * *')
   async handleCron() {
+    let tasks = await this.getTasks();
+    let messages = this.prepareMessages(tasks);
+    this.sendPushNotifications(messages);
+  }
+
+  async getTasks() {
     const startDate = new Date();
     const endDate = new Date(new Date().setUTCMinutes(startDate.getUTCMinutes() + 5));
+    return await this.taskModel
+      .find(
+        {start_date: {$gte: startDate, $lt: endDate}},
+        null,
+        {sort: {start_date: 1}, populate: 'user'}
+      )
+      .exec();
+  }
 
-    let tasks = await this.taskModel
-      .find({
-          start_date: {
-            $gte: startDate,
-            $lt: endDate,
-          }
-        }, null,
-        {
-          sort: {start_date: 1},
-          populate: 'user',
-        }
-      );
+  prepareMessages(tasks: Array<Task>): Array<ExpoPushMessage> {
+    return tasks.map(task => ({
+      to: task.user.push_notification_token,
+      sound: 'default',
+      body: `"${task.name}" task scheduled for the near future`,
+      data: {withSome: 'data'},
+    }));
+  }
 
+  async sendPushNotifications(messages: Array<ExpoPushMessage>) {
+    let expo = new Expo();
+    let chunks = expo.chunkPushNotifications(messages);
+
+    for (const chunk of chunks) {
+      try {
+        await expo.sendPushNotificationsAsync(chunk);
+      } catch (error) {
+        throw error;
+      }
+    }
   }
 }
