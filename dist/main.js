27bf48b1c9c06f3ff61c7e6a667b331f1230114a
statkevich <statkevich.ivan9999@gmail.com>
2023-08-25 15:11:05 +0300

    changed port

--- dist/main.js
+++ dist/main.js
@@ -4,12 +4,13 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const dotenv_1 = require("dotenv");
 const common_1 = require("@nestjs/common");
+const process = require("process");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     (0, dotenv_1.config)();
     app.setGlobalPrefix('v1');
     app.useGlobalPipes(new common_1.ValidationPipe());
-    await app.listen(3005);
+    await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {config} from 'dotenv';
 import {ValidationPipe} from '@nestjs/common';
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   config();
   app.setGlobalPrefix('v1');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT,  '0.0.0.0');
 }
 
 bootstrap();
